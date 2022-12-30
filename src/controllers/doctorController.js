6add58dc26329c9242fb839e2f077401961f1019
NguyenPhu <ngphu1601@gmail.com>
2022-12-30 18:54:40 +0700

    update database 30/12

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -16,6 +16,48 @@ let getTopDoctorHome = async (req, res) => {
   }
 };
 
+let getAllDoctors = async (req, res) => {
+  try {
+    let response = await doctorService.getAllDoctors();
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from server...",
+    });
+  }
+};
+
+let postInfoDoctor = async (req, res) => {
+  try {
+    let response = await doctorService.saveDetailInfoDoctor(req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from server...",
+    });
+  }
+};
+
+let getDetailDoctorById = async (req, res) => {
+  try {
+    let info = await doctorService.getDetailDoctorById(req.query.id);
+    return res.status(200).json(info);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from server...",
+    });
+  }
+};
+
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
+  getAllDoctors: getAllDoctors,
+  postInfoDoctor: postInfoDoctor,
+  getDetailDoctorById: getDetailDoctorById,
 };
